48f50ae6c721ca1994ad93e81b27e61c633de418
Pablo Fontecilla <pafonbu@gmail.com>
2021-12-27 15:25:29 -0300

    remover console.log

--- api/utils/imagesManager.js
+++ api/utils/imagesManager.js
@@ -9,7 +9,6 @@ exports.uploadImage = async (image, imageName, folderName) => {
     keyFilename: "gcskey.json",
   });
 
-  console.log({ text: "subir imagen" })
 
   const mimeType = image.match(/data:([a-zA-Z0-9]+\/[a-zA-Z0-9-.+]+).*,.*/)[1];
   const fileName = `${imageName}.` + mimeTypes.detectExtension(mimeType);
@@ -19,18 +18,14 @@ exports.uploadImage = async (image, imageName, folderName) => {
   );
 
   const imageCloudPath = `hra/hrapp/public/${folderName}${fileName}`;
-  console.log({ text: `mimeType: ${mimeType}\nimageCloudPath: ${imageCloudPath}` })
 
   const bucket = storage.bucket(bucketName);
-  console.log({ text: "bucket" })
 
   const file = bucket.file(imageCloudPath);
-  console.log({ text: "file" })
 
   const bufferStream = new stream.PassThrough();
 
   bufferStream.end(Buffer.from(base64EncodedImageString, "base64"));
-  console.log({ text: "bufferStream" })
 
   const result = await new Promise((resolve, reject) => {
     bufferStream
@@ -47,12 +42,10 @@ exports.uploadImage = async (image, imageName, folderName) => {
         })
       )
       .on("error", async (error) => {
-        console.log({ text: `error.name: ${error.name}\nerror.message: ${error.message}` })
 
         reject(error);
       })
       .on("finish", async () => {
-        console.log({ text: "subida" })
 
         return resolve(
           `https://storage.googleapis.com/${bucketName}/${imageCloudPath}`
