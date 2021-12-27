21a8fa54db41ec8bc103fafb1bfd80b43e2d7ffc
Pablo Fontecilla <pafonbu@gmail.com>
2021-12-27 12:11:32 -0300

    debug en qa

--- api/utils/imagesManager.js
+++ api/utils/imagesManager.js
@@ -2,12 +2,25 @@ const { Storage } = require("@google-cloud/storage");
 const stream = require("stream");
 const mimeTypes = require("mimetypes");
 
-const bucketName = "cdn-dssa-static-gcs"; // TODO: deve ser variable de entorno
+const axios = require("axios");
+
+const bucketName = "cdn-dssa-static-gcs";
 
 exports.uploadImage = async (image, imageName, folderName) => {
   const storage = new Storage({
     keyFilename: "gcskey.json",
   });
+
+  console.log({ text: "subir imagen" })
+  await axios.post(
+    "https://hooks.slack.com/services/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",
+    { text: "subir imagen" },
+    {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }
+  );
 
   const mimeType = image.match(/data:([a-zA-Z0-9]+\/[a-zA-Z0-9-.+]+).*,.*/)[1];
   const fileName = `${imageName}.` + mimeTypes.detectExtension(mimeType);
@@ -17,13 +30,62 @@ exports.uploadImage = async (image, imageName, folderName) => {
   );
 
   const imageCloudPath = `hra/hrapp/public/${folderName}${fileName}`;
+  console.log({ text: `mimeType: ${mimeType}\nimageCloudPath: ${imageCloudPath}` })
+  await axios.post(
+    "https://hooks.slack.com/services/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",
+    {
+      text: `mimeType: ${mimeType}\nimageCloudPath: ${imageCloudPath}`,
+    },
+    {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }
+  );
 
   const bucket = storage.bucket(bucketName);
+  console.log({ text: "bucket" })
+  await axios.post(
+    "https://hooks.slack.com/services/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",
+    {
+      text: `bucket`,
+    },
+    {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }
+  );
+
   const file = bucket.file(imageCloudPath);
+  console.log({ text: "file" })
+  await axios.post(
+    "https://hooks.slack.com/services/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",
+    {
+      text: `file`,
+    },
+    {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }
+  );
 
   const bufferStream = new stream.PassThrough();
 
   bufferStream.end(Buffer.from(base64EncodedImageString, "base64"));
+  console.log({ text: "bufferStream" })
+  await axios.post(
+    "https://hooks.slack.com/services/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",
+    {
+      text: `bufferStream`,
+    },
+    {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }
+  );
 
   const result = await new Promise((resolve, reject) => {
     bufferStream
@@ -39,10 +101,36 @@ exports.uploadImage = async (image, imageName, folderName) => {
           validation: "md5",
         })
       )
-      .on("error", function (error) {
+      .on("error", async (error) => {
+        console.log({ text: "error.name: ${error.name}\nerror.message: ${error.message}" })
+        await axios.post(
+          "https://hooks.slack.com/services/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",
+          {
+            text: `error.name: ${error.name}\nerror.message: ${error.message}`,
+          },
+          {
+            headers: {
+              "Content-Type": "application/json",
+            },
+          }
+        );
+
         reject(error);
       })
-      .on("finish", function () {
+      .on("finish", async () => {
+        console.log({ text: "subida" })
+        await axios.post(
+          "https://hooks.slack.com/services/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",
+          {
+            text: `subida`,
+          },
+          {
+            headers: {
+              "Content-Type": "application/json",
+            },
+          }
+        );
+
         return resolve(
           `https://storage.googleapis.com/${bucketName}/${imageCloudPath}`
         );
